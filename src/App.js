6c9cdbfd626afc2a62d62dbe0e54ffc7e16ed7e6
Bermet <oceaninapuddle@gmail.com>
2023-01-18 16:05:21 +0600

    user search created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 import { Success } from "./components/Success";
-import { Users } from "./components/Users";
-
-// Тут список пользователей: https://reqres.in/api/users
+import { Users } from "./components/Users/Users";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    fetch(" https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const ovChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        ovChangeSearchValue={ovChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
       {/* <Success /> */}
     </div>
   );
